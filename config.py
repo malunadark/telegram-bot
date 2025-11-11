119fab1df819991e8df30efc06110285509f64d8
Татьяна <93919072+malunadark@users.noreply.github.com>
2025-11-11 23:30:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
 
-IMAGES_DIR = "images/horror"
-QUEST_FILE = "horror_quests.json"
+BASE_DIR = os.path.dirname(__file__)
+ASSETS_DIR = os.path.join(BASE_DIR, 'assets', 'General')
+IMAGES_DIR = os.path.join(BASE_DIR, 'assets', 'Images')
+QUEST_FILE = os.path.join(BASE_DIR, 'quests.json')
