5e99831cf52fe88ff242187fe6e1603db83e291d
Татьяна <93919072+malunadark@users.noreply.github.com>
2025-11-27 21:22:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
-BASE_DIR = os.path.dirname(__file__)
-ASSETS_DIR = os.path.join(BASE_DIR, 'assets', 'General')
-IMAGES_DIR = os.path.join(BASE_DIR, 'assets', 'Images')
-QUEST_FILE = os.path.join(BASE_DIR, 'quests.json')
+
+TOKEN = os.environ.get("BOT_TOKEN")   # Render.com → Environment Variables
+GROUP_ID = -1001234567890             # замени на свой
+QUEST_FILE = "data/quests.json"
+IMAGES_DIR = "data/images"
