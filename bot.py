69bfe7aaf86701c0da6cefa32dc2c6bf04f95956
Татьяна <93919072+malunadark@users.noreply.github.com>
2026-02-24 16:54:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,46 @@
 import asyncio
-from aiogram import Bot, Dispatcher
-from config import BOT_TOKEN
-from database import connect
-from handlers import start, episode1
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.enums import ParseMode
+from aiogram import Router
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher()
+router = Router()
+dp.include_router(router)
+
+
+# 🌫 Приветствие новых участников
+@router.message(F.new_chat_members)
+async def welcome_new_member(message: Message):
+    for member in message.new_chat_members:
+        
+        # Удаляем системное сообщение "X joined the group"
+        try:
+            await message.delete()
+        except:
+            pass
+
+        await asyncio.sleep(1.5)
+
+        await message.answer(
+            f"🌫 <b>{member.full_name}</b> Вошёл в туман Nostai.\n\n"
+            "Дым здесь гуще, чем кажется.\n"
+            "Слова — не всегда правда.\n"
+            "Выбор — только за тобой.\n\n"
+            "Будь внимателен."
+        )
+
 
 async def main():
-    await connect()
-
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
-
-    dp.include_router(start.router)
-    dp.include_router(episode1.router)
-
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
